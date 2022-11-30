d1a6ace6cee96ec5e2e8a7574c6dded92ea9c802
İsmail Yağcı <ismailyagci@Ismail-MBP.home>
2022-11-30 10:38:38 +0300

    Java Log

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(echo ""; echo "org.gradle.java.home=${JAVA_HOME_11_X64}") >> $APPCENTER_SOURCE_DIRECTORY/android/gradle.properties
+(echo ""; echo "Java Home: ${JAVA_HOME_11_X64}")
